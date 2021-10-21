95a01e34e658cb984b5381843df0af964792c1fe
Loris D'Antonio <loris.dantonio@gmail.com>
2021-10-21 16:30:49 +0200

    EC2 Allow Traffic Only from ALB | NAT Gateway outbound for internet Downloads

--- applications/wordpress/modules/webserver/webserver.tf
+++ applications/wordpress/modules/webserver/webserver.tf
@@ -1,14 +1,7 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound connections"
+resource "aws_security_group" "allow_http_out" {
+  name        = "allow_http_out"
+  description = "Allow HTTP outbound connections"
   vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port       = 0
@@ -18,7 +11,7 @@ resource "aws_security_group" "allow_http" {
   }
 
   tags = {
-    Name = "Allow HTTP Security Group"
+    Name = "Allow HTTP Outbound"
   }
 }
 
@@ -31,7 +24,7 @@ resource "aws_security_group" "allow_from_lb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.allow_http.id]
+    security_groups = [aws_security_group.lb_http.id]
   }
 
   tags = {
@@ -49,7 +42,7 @@ resource "aws_launch_template" "web" {
   instance_type = "t2.micro"
 
   key_name = aws_key_pair.web.key_name
-  vpc_security_group_ids = [aws_security_group.allow_from_lb.id]
+  vpc_security_group_ids = [aws_security_group.allow_from_lb.id,aws_security_group.allow_http_out.id]
 
   instance_market_options {
     market_type = "spot"
